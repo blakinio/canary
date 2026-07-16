27e4d3e5a056cbab94f4ca7a715a287a0cd5e94d
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-16 18:57:47 +0200

    fix(lua): preserve isolated analytics extension loading

--- data-otservbr-global/scripts/lib/gameplay_analytics_correctness.lua
+++ data-otservbr-global/scripts/lib/gameplay_analytics_correctness.lua
@@ -1,1 +1,5 @@
+if GameplayAnalytics and not GameplayAnalytics.coreLoaded and not GameplayAnalytics.loadComplete then
+	return dofile("data-otservbr-global/scripts/lib/#gameplay_analytics_correctness_impl.lua")
+end
+
 return dofile("data-otservbr-global/scripts/lib/gameplay_analytics.lua")
--- data-otservbr-global/scripts/lib/gameplay_analytics_reliability.lua
+++ data-otservbr-global/scripts/lib/gameplay_analytics_reliability.lua
@@ -1,1 +1,5 @@
+if GameplayAnalytics and not GameplayAnalytics.coreLoaded and not GameplayAnalytics.loadComplete then
+	return dofile("data-otservbr-global/scripts/lib/#gameplay_analytics_reliability_impl.lua")
+end
+
 return dofile("data-otservbr-global/scripts/lib/gameplay_analytics.lua")
