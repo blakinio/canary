c6ddd4b979da10ded23948f0bffec62ad877501d
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-16 18:37:10 +0200

    fix(lua): centralize gameplay analytics extension loading

--- data-otservbr-global/scripts/lib/gameplay_analytics.lua
+++ data-otservbr-global/scripts/lib/gameplay_analytics.lua
@@ -1,7 +1,18 @@
-if GameplayAnalytics and GameplayAnalytics.coreLoaded then
+if GameplayAnalytics and GameplayAnalytics.loadComplete then
 	return GameplayAnalytics
 end
 
-local Analytics = dofile("data-otservbr-global/scripts/lib/#gameplay_analytics_core.lua")
-Analytics.coreLoaded = true
+local Analytics = GameplayAnalytics
+if not Analytics or not Analytics.coreLoaded then
+	Analytics = dofile("data-otservbr-global/scripts/lib/#gameplay_analytics_core.lua")
+	Analytics.coreLoaded = true
+end
+
+dofile("data-otservbr-global/scripts/lib/#gameplay_analytics_batching_impl.lua")
+dofile("data-otservbr-global/scripts/lib/#gameplay_analytics_context_impl.lua")
+dofile("data-otservbr-global/scripts/lib/#gameplay_analytics_correctness_impl.lua")
+dofile("data-otservbr-global/scripts/lib/#gameplay_analytics_reliability_impl.lua")
+dofile("data-otservbr-global/scripts/lib/#gameplay_analytics_schema_impl.lua")
+
+Analytics.loadComplete = true
 return Analytics
--- data-otservbr-global/scripts/lib/gameplay_analytics_correctness.lua
+++ data-otservbr-global/scripts/lib/gameplay_analytics_correctness.lua
@@ -1,6 +1,1 @@
-local Analytics = GameplayAnalytics
-if not Analytics or not Analytics.coreLoaded then
-	Analytics = dofile("data-otservbr-global/scripts/lib/gameplay_analytics.lua")
-end
-
-return dofile("data-otservbr-global/scripts/lib/#gameplay_analytics_correctness_impl.lua")
+return dofile("data-otservbr-global/scripts/lib/gameplay_analytics.lua")
